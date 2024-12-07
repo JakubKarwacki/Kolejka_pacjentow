6eccde2351c90f9266e301dca0dbf3eec0675220
Jakub Karwacki <awdykowicz.jakub@gmail.com>
2024-12-07 15:05:49 +0100

    Add initial implementation of patient queue with basic structure and functions

---
+++ main.c
@@ -0,0 +1,20 @@
+/*
+Jakub Karwacki, WME23BX1S1
+Data utworzenia: 2024.12.07, rozliczono: 2024.12.07
+Zadanie 1: Kolejka Pacjentów - FIFO
+*/
+
+// Files
+    #include "main.h"
+
+// Functions
+    int addPatient() {
+
+        return 0;
+    }
+
+    int main() {
+        printf("Hello, World!\n");
+
+        return 0;
+    }
---
+++ main.h
@@ -0,0 +1,16 @@
+/*
+Jakub Karwacki, WME23BX1S1
+Data utworzenia: 2024.12.07, rozliczono: 2024.12.07
+Zadanie 1: Kolejka Pacjentów - FIFO
+*/
+
+// Libraries
+    #include <stdio.h>
+
+// Variables
+
+// Structures
+
+// Functions
+    int addPatient();
+    int main();
